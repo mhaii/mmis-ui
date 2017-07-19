3da7a9238a4596e3b947976d1f3df324460ac0d5
mhaii <ppuri.yc@gmail.com>
2017-07-20 04:11:05 +0700

    remove redux, add monitor dev scripts

--- views/src/containers/list_view.js
+++ views/src/containers/list_view.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { selectItem } from '../actions/list_actions';
-
 
 class ListView extends Component {
     renderList() {
@@ -30,30 +26,4 @@ class ListView extends Component {
     }
 }
 
-
-/*
-This is a redux specific function.
-What is does is: It gets the state specified in here from the global redux state.
-For example, here we are retrieving the list of items from the redux store.
-Whenever this list changes, any component that is using this list of item will re-render.
- */
-function mapStateToProps(state) {
-    return {
-        lists: state.lists.all
-    };
-}
-
-/*
-This is a redux specific function.
-http://redux.js.org/docs/api/bindActionCreators.html
- */
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators({ selectItem: selectItem }, dispatch);
-}
-
-
-/*
-Here we are creating a Higher order component
-https://facebook.github.io/react/docs/higher-order-components.html
- */
-export default connect(mapStateToProps, mapDispatchToProps)(ListView);
+export default ListView;
